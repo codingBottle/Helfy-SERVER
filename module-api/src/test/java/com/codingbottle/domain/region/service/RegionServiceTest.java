fcb88f4e39921c65c1997397205c005220426417
hongyoung <qwe916@naver.com>
2023-10-24 23:10:53 +0900

    test: add user region update service test

--- module-api/src/test/java/com/codingbottle/domain/region/service/RegionServiceTest.java
+++ module-api/src/test/java/com/codingbottle/domain/region/service/RegionServiceTest.java
@@ -1,5 +1,7 @@
 package com.codingbottle.domain.region.service;
 
+import com.codingbottle.auth.entity.User;
+import com.codingbottle.domain.region.entity.Region;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +25,23 @@ class RegionServiceTest {
 
         // then
         assertThat(regions).isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("사용자 지역 수정")
+    void update_user_regions() {
+        // given
+        var region = Region.SEOUL;
+
+        User user = User.builder()
+                .region(Region.NONE)
+                .build();
+
+        // when
+        var updateUser = regionService.updateRegions(region, user);
+
+        // then
+        assertThat(updateUser.getRegion()).isEqualTo(region);
     }
 
 }
