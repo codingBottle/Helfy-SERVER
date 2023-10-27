84e51e096109eff14dcb4b3597ee19eca602d6bd
hongyoung <qwe916@naver.com>
2023-10-27 17:30:30 +0900

    refactor: add error response and refactor method

--- module-core/src/main/java/com/codingbottle/common/security/filter/FirebaseTokenFilter.java
+++ module-core/src/main/java/com/codingbottle/common/security/filter/FirebaseTokenFilter.java
@@ -2,9 +2,11 @@ package com.codingbottle.common.security.filter;
 
 import com.codingbottle.auth.entity.Role;
 import com.codingbottle.auth.entity.User;
+import com.codingbottle.auth.service.UserDetailService;
 import com.codingbottle.common.exception.ApplicationErrorException;
 import com.codingbottle.common.exception.ApplicationErrorType;
-import com.codingbottle.auth.service.UserDetailService;
+import com.codingbottle.common.model.ErrorResponseDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
@@ -14,6 +16,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -30,28 +33,63 @@ public class FirebaseTokenFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         String header = request.getHeader("Authorization");
-        if (header == null || !header.startsWith("Bearer ")) {
-            throw new ApplicationErrorException(ApplicationErrorType.INVALID_HEADER, "Authorization 헤더가 없거나 Bearer 토큰이 아닙니다.");
+
+        if (isInvalidHeader(header)) {
+            log.error("requestURL: {}, Invalid header: {}", request.getRequestURI(), header);
+            handleFirebaseError(response, ApplicationErrorType.INVALID_HEADER);
+            return;
         }
+
         String token = header.substring(7);
 
-        try{
+        try {
             FirebaseToken firebaseToken = firebaseAuth.verifyIdToken(token);
+            log.debug("requestURL: {},Valid firebaseToken: {}", request.getRequestURI(), firebaseToken);
+            User user = getUser(firebaseToken);
 
-            User user;
+            authenticateUser(user);
 
-            try {
-                user = userService.updateByUsername(firebaseToken);
-            } catch (ApplicationErrorException e) {
-                user = userService.create(firebaseToken, Role.ROLE_USER);
-            }
-
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-
-            SecurityContextHolder.getContext().setAuthentication(authentication);
         } catch (FirebaseAuthException e) {
-            throw new ApplicationErrorException(ApplicationErrorType.INVALID_FIREBASE_TOKEN);
+            log.error("requestURL: {}, Invalid FirebaseToken: {}", request.getRequestURI(), header);
+            handleFirebaseError(response, ApplicationErrorType.INVALID_FIREBASE_TOKEN);
+            return;
         }
         filterChain.doFilter(request, response);
+    }
+
+    private boolean isInvalidHeader(String header) {
+        return header == null || !header.startsWith("Bearer ");
+    }
+
+    private User getUser(FirebaseToken firebaseToken) {
+        User user;
+        try {
+            user = userService.updateByUsername(firebaseToken);
+        } catch (ApplicationErrorException e) {
+            user = userService.create(firebaseToken, Role.ROLE_USER);
+        }
+        return user;
+    }
+
+    private void authenticateUser(User user) {
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private void handleFirebaseError(HttpServletResponse response, ApplicationErrorType applicationErrorType) throws IOException {
+        setErrorResponseHeader(HttpStatus.UNAUTHORIZED, response);
+        setErrorResponseBody(response, applicationErrorType);
+    }
+
+    private void setErrorResponseHeader(HttpStatus status, HttpServletResponse response) throws IOException {
+        response.setStatus(status.value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+    }
+
+    private void setErrorResponseBody(HttpServletResponse response, ApplicationErrorType applicationErrorType) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(applicationErrorType.name(), applicationErrorType.getMessage());
+        objectMapper.writeValue(response.getWriter(), errorResponseDto);
     }
 }
