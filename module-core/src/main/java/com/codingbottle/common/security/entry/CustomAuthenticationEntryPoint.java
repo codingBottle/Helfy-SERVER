39fb169d33078579f7bf6347bee02799879a0828
Hong <77445491+qwe916@users.noreply.github.com>
2023-11-01 23:23:08 +0900

    Slack 로그백 추가 및 로깅  (#27)
    * feat: add AccessDeniedHandler
    
    * refactor: add error response and refactor method
    
    * chore: add slack logback dependency
    
    * docs: add logback xml
    
    * fix: modify logback-spring.xml

--- module-core/src/main/java/com/codingbottle/common/security/entry/CustomAuthenticationEntryPoint.java
+++ module-core/src/main/java/com/codingbottle/common/security/entry/CustomAuthenticationEntryPoint.java
@@ -1,15 +1,16 @@
-package com.codingbottle.common.security;
+package com.codingbottle.common.security.entry;
 
 import com.codingbottle.common.exception.ApplicationErrorType;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
