3c3181aa9f38271db378c373d059f8fc582622cd
Cho, Yonghyun <chooh1010@gamil.com>
2023-11-01 16:20:17 +0900

    feat: jpa auditing

---
+++ module-core/src/main/java/com/codingbottle/domain/objectTIme/entity/ObjectTime.java
@@ -0,0 +1,27 @@
+package com.codingbottle.domain.objectTIme.entity;
+
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public abstract class ObjectTime {
+
+    @CreatedDate
+    private LocalDateTime createTime;
+
+    @LastModifiedDate
+    private LocalDateTime updateTime;
+}
