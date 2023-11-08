5a5fd8d21cee37578ce5c35840fdb4e296f8e1ae
Cho, Yonghyun <chooh1010@gamil.com>
2023-11-08 14:47:42 +0900

    dto를 record로 변환

--- module-api/src/main/java/com/codingbottle/domain/post/dto/UpdatePostRequest.java
+++ module-api/src/main/java/com/codingbottle/domain/post/dto/UpdatePostRequest.java
@@ -1,18 +1,11 @@
 package com.codingbottle.domain.post.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Builder
-public class UpdatePostRequest {
-    @NotBlank
-    private String content;
-    @NotBlank
-    private Long imageId;
+public record UpdatePostRequest (
+        @NotBlank
+        String content,
+        @NotBlank
+        Long imageId
+){
 }
