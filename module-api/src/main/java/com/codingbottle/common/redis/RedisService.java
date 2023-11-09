42d74b0dacdb0076070b93cb5463e815adaffbaf
hongyoung <qwe916@naver.com>
2023-11-09 22:07:06 +0900

    feat: add Redis service logic

---
+++ module-api/src/main/java/com/codingbottle/common/redis/RedisService.java
@@ -0,0 +1,36 @@
+package com.codingbottle.common.redis;
+
+import com.codingbottle.domain.region.entity.Region;
+import com.codingbottle.domain.weather.model.WeatherResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+    private final RedisTemplate<String, String> redisTemplate;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public void setObjectValues(Region key, WeatherResponse value) throws JsonProcessingException {
+        String mapperK = writeKeyAsString(key);
+        var mapperV = objectMapper.writeValueAsString(value);
+
+        redisTemplate.opsForValue().set(mapperK, mapperV, 3600000, TimeUnit.MILLISECONDS);
+    }
+
+    public WeatherResponse getObjectValues(Region key) throws JsonProcessingException {
+        String mapperK = writeKeyAsString(key);
+        String value = redisTemplate.opsForValue().get(mapperK);
+
+        return objectMapper.readValue(value, WeatherResponse.class);
+    }
+
+    private String writeKeyAsString(Region key) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(key);
+    }
+}
