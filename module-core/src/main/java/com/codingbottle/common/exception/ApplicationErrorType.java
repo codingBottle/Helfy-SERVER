19f785e12614d38171314ddc3d55f2b0ad4a06d2
hongyoung <qwe916@naver.com>
2024-01-12 14:09:42 +0900

    feat: add global exception handler(InvalidDataAccess exception)

--- module-core/src/main/java/com/codingbottle/common/exception/ApplicationErrorType.java
+++ module-core/src/main/java/com/codingbottle/common/exception/ApplicationErrorType.java
@@ -13,6 +13,7 @@ public enum ApplicationErrorType {
     INVALID_FIREBASE_TOKEN(HttpStatus.BAD_REQUEST, "Firebase 토큰이 올바르지 않습니다."),
     INVALID_FILE_TYPE(HttpStatus.BAD_REQUEST, "파일 타입이 올바르지 않습니다."),
     NOT_EXIT_WRONG_ANSWER(HttpStatus.BAD_REQUEST, "틀린 문제가 없습니다."),
+    INVALID_DATA_ACCESS(HttpStatus.BAD_REQUEST, "데이터 접근이 올바르지 않습니다."),
     //401
     NO_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
     //403
@@ -30,7 +31,8 @@ public enum ApplicationErrorType {
     FAIL_TO_UPLOAD_IMAGE(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드에 실패하였습니다"),
     REDIS_DELETE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Redis 삭제에 실패하였습니다."),
     INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 문제가 발생했습니다. 잠시만 기달려주세요."),
-    JSON_PROCESSING_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "JSON 처리 중 에러가 발생했습니다." );
+    JSON_PROCESSING_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "JSON 처리 중 에러가 발생했습니다."),
+    ;
 
 
     private final HttpStatus httpStatus;
