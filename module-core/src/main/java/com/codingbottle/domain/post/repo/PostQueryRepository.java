ef94e48b740ab2289dd8ce9c39a7f321f4d874d1
hongyoung <qwe916@naver.com>
2024-01-12 14:13:40 +0900

    feat: implemented querying all posts with pagination using QueryDSL

--- module-core/src/main/java/com/codingbottle/domain/post/repo/PostQueryRepository.java
+++ module-core/src/main/java/com/codingbottle/domain/post/repo/PostQueryRepository.java
@@ -2,8 +2,11 @@ package com.codingbottle.domain.post.repo;
 
 import com.codingbottle.domain.post.entity.Post;
 import com.codingbottle.domain.post.entity.QPost;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,11 +15,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PostQueryRepository {
     private final JPAQueryFactory jpaQueryFactory;
-    private final QPost qPost = QPost.post;
+    private final QPost post = QPost.post;
 
     public List<Post> searchByKeyword(String keyword) {
-        return jpaQueryFactory.selectFrom(qPost)
-                .where(qPost.hashtags.contains(keyword))
+        return jpaQueryFactory.selectFrom(post)
+                .where(post.hashtags.contains(keyword))
+                .fetch();
+    }
+
+    public List<Post> finAll(Pageable pageable) {
+        return jpaQueryFactory.selectFrom(post)
+                .limit(pageable.getPageSize())
+                .offset(pageable.getOffset())
+                .orderBy(new OrderSpecifier[]{new OrderSpecifier<>(Order.DESC, post.createdTime)})
                 .fetch();
     }
 }
