3aa9d8119c481df5662316417bad94b4c62a492d
hongyoung <qwe916@naver.com>
2024-01-14 09:54:48 +0900

    feat: applied caching to user authentication process

--- module-api/src/main/java/com/codingbottle/common/security/service/UserDetailService.java
+++ module-api/src/main/java/com/codingbottle/common/security/service/UserDetailService.java
@@ -1,16 +1,17 @@
-package com.codingbottle.auth.service;
+package com.codingbottle.common.security.service;
 
-import com.codingbottle.auth.entity.Role;
-import com.codingbottle.auth.entity.User;
-import com.codingbottle.auth.repository.UserRepository;
-import com.codingbottle.common.exception.ApplicationErrorException;
-import com.codingbottle.common.exception.ApplicationErrorType;
+import com.codingbottle.domain.user.entity.Role;
+import com.codingbottle.domain.user.entity.User;
+import com.codingbottle.domain.user.repository.UserRepository;
 import com.codingbottle.domain.region.entity.Region;
 import com.google.firebase.auth.FirebaseToken;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
@@ -22,31 +23,36 @@ public class UserDetailService implements UserDetailsService {
 
     @Override
     @Transactional(readOnly = true)
-    public User loadUserByUsername(String firebaseUid) throws UsernameNotFoundException {
-        return userRepository.findByFirebaseUid(firebaseUid)
+    public UserDetails loadUserByUsername(String firebaseUid) throws UsernameNotFoundException {
+        User user = userRepository.findByFirebaseUid(firebaseUid)
                 .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다."));
+
+        return new SecurityUserDetails(user);
     }
 
     @Transactional
-    public User create(FirebaseToken firebaseToken, Role role) {
+    @CachePut(value = "user", key = "#firebaseToken.uid", cacheManager = "authRedisCacheManager")
+    public User create(FirebaseToken firebaseToken, String token) {
         User user = User.builder()
                 .firebaseUid(firebaseToken.getUid())
                 .email(firebaseToken.getEmail())
                 .nickname("user" + UUID.randomUUID())
                 .region(Region.NONE)
-                .role(role)
+                .role(Role.ROLE_USER)
                 .build();
 
-        return userRepository.save(user);
+       return userRepository.save(user);
     }
 
     @Transactional
+    @Cacheable(value = "user", key = "#firebaseToken.uid", cacheManager = "authRedisCacheManager")
     public User updateByUsername(FirebaseToken firebaseToken) {
         User user = userRepository.findByFirebaseUid(firebaseToken.getUid())
-                .orElseThrow(() -> new ApplicationErrorException(ApplicationErrorType.USER_NOT_FOUND, String.format("해당 유저(%s)를 찾을 수 없습니다.", firebaseToken.getEmail())));
+                .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다."));
 
-        user.update(firebaseToken);
+        user.updateByFirebaseToken(firebaseToken.getUid(), firebaseToken.getEmail());
 
         return userRepository.save(user);
     }
+
 }
